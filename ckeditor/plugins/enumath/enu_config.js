2d783b0e1ad6194d28977cfd51709d4a6f278453
Tomas Bednarik <bednarik.tomas1@gmail.com>
2024-12-03 10:35:10 +0100

    clear unused variables from config

--- ckeditor/plugins/enumath/enu_config.js
+++ ckeditor/plugins/enumath/enu_config.js
@@ -4,7 +4,4 @@
 	EQUATION_ENGINE = http+'latex.codecogs.com';
 	EQUATION_ENGINE_CODECOGS = http+'latex.codecogs.com';
 	EQUATION_ENGINE_VERCEL    = http+'math.vercel.app';
-	FAVORITE_ENGINE = http+'latex.codecogs.com/json';
-	EDITOR_SRC      = http+'latex.codecogs.com';
-	EDIT_ENGINE     = http+'www.codecogs.com/eqnedit.php';
 }
